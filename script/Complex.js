0851925a893ce1bb1731371cd730e4d14285090a
Yihang Ho <hoyihang5@gmail.com>
2014-02-12 18:06:06 +0800

    Inital commit. Add the Complex class.

---
+++ script/Complex.js
@@ -0,0 +1,69 @@
+// This complex class
+function Complex(re, im) {
+	this.re = re;
+	this.im = im;
+
+	if (typeof this.add != "function") {
+		Complex.prototype.add = function(another) {
+			this.re += another.re;
+			this.im += another.im;
+			return this;
+		};
+
+		Complex.prototype.minus = function(another) {
+			this.re -= another.re;
+			this.im -= another.im;
+			return this;
+		};
+
+		Complex.prototype.multiply = function(another) {
+			var re = this.re * another.re - this.im * another.im;
+			var im = this.re * another.im + this.im * another.re;
+			this.re = re;
+			this.im = im;
+			return this;
+		};
+
+		Complex.prototype.divide = function(another) {
+			var denominator = another.re * another.re + another.im * another.im;
+			this.multiply(another.conjugate());
+			this.re /= denominator;
+			this.im /= denominator;
+			return this;
+		};
+
+		Complex.prototype.modulus = function() {
+			var a2 = this.re * this.re;
+			var b2 = this.im * this.im;
+			return Math.sqrt(a2 + b2);
+		};
+
+		Complex.prototype.argument = function() {
+			return Math.atan2(this.im, this.re);
+		};
+
+		Complex.prototype.polar = function() {
+			return "(" + this.modulus() + ", " + this.argument() + " rad)";
+		};
+
+		Complex.prototype.conjugate = function() {
+			this.im *= -1;
+			return this;
+		};
+
+		Complex.prototype.toString = function() {
+			if (this.re && this.im) {
+				if (this.im >= 0) {
+					return this.re + " + " + this.im + "i";
+				} else {
+					return this.re + " - " + -1 * this.im + "i";
+				}
+				
+			} else if (this.im) {
+				return this.im + "i";
+			} else {
+				return this.re;
+			}
+		};
+	}
+}
