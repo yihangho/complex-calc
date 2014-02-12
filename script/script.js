7bba2ce898ca1e369521a026210c1991a4e5f15b
Yihang Ho <hoyihang5@gmail.com>
2014-02-12 19:20:36 +0800

    Add basic UI.

---
+++ script/script.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", function() {
+	console.log("Hello");
+	var submitBtn = document.getElementById("submit");
+	console.log(submitBtn);
+	submitBtn.addEventListener("click", function() {
+		var inputBox = document.getElementById("input");
+		var input = inputBox.value;
+		var output = execute(parse(input)).toString();
+		logIO(input, output);
+	});
+
+	function logIO(input, output) {
+		var ul = document.getElementById("io-log");
+		var li = document.createElement("li");
+		var inputDiv = document.createElement("div");
+		var outputDiv = document.createElement("div");
+		inputDiv.appendChild(document.createTextNode("Input: " + input));
+		outputDiv.appendChild(document.createTextNode("Output: " + output));
+		li.appendChild(inputDiv);
+		li.appendChild(outputDiv);
+		ul.appendChild(li);
+	}
+})
